18a15be477302980ea402f246aa8854029d648fa
Ratna Priya <37251540+Ratna04priya@users.noreply.github.com>
2019-03-26 08:03:38 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,5 +1,14 @@
 import sys
 from PyQt4 import QtGui, QtCore
+from PyQt4 import QtGui
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import style
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 
 class Window(QtGui.QMainWindow):
 
@@ -16,7 +25,7 @@ class Window(QtGui.QMainWindow):
         a.triggered.connect(self.editor)
         #a.triggered.connect(self.close_application)
 
-        openFile = QtGui.QAction("&Open File", self)
+        openFile = QtGui.QAction("&Load", self)
         openFile.setShortcut("Ctrl+O")
         openFile.setStatusTip('Open File')
         openFile.triggered.connect(self.file_open)
@@ -37,6 +46,12 @@ class Window(QtGui.QMainWindow):
         d.setShortcut("Ctrl+Q")
         d.setStatusTip('Wanna leave the App')
         d.triggered.connect(self.close_application)
+
+        p =  QtGui.QAction("&Plot", self)
+        p.setShortcut("Ctrl+P")
+        p.setStatusTip('Plot')
+        p.triggered.connect(self.plot)
+
         
 
 
@@ -53,6 +68,9 @@ class Window(QtGui.QMainWindow):
         viewMenu = mainMenu.addMenu('&Edit')
         viewMenu.addAction(openEditor)
 
+        plotmenu= mainMenu.addMenu('&Plot')
+        plotmenu.addAction(p)
+
         exit = mainMenu.addMenu('&Exit')
         exit.addAction(d)
 
@@ -68,9 +86,18 @@ class Window(QtGui.QMainWindow):
         btn.move(100,100)
 
         self.toolBar = self.addToolBar("C")
+    
+
+
 
     def file_open(self):
-        name = QtGui.QFileDialog.getOpenFileName(self, 'Open File')
+        name = QtGui.QFileDialog.getOpenFileName(self,
+                                                    'CSV File',
+                                                   '/home/ratna/Documents/GUI_p/csvfiles','*.csv')
+        file=pd.read_csv(name)
+        #self.tableView.setModel(file)
+        print(file)
+
         file = open(name,'r')
 
         self.editor()
@@ -78,6 +105,34 @@ class Window(QtGui.QMainWindow):
         with file:
             text = file.read()
             self.textEdit.setText(text)
+
+    def plot(self):
+        name = QtGui.QFileDialog.getOpenFileName(self,
+                                                    'CSV File',
+                                                   '/home/ratna/Documents/GUI_p/csvfiles','*.csv')
+        file=pd.read_csv(name)
+        
+        print(file)
+        print(file.columns)
+
+        text1, ok = QInputDialog.getText(self, 'Text Input Dialog', 'Enter first column:')
+        text2, oku = QInputDialog.getText(self, 'Text Input Dialog', 'Enter second column:')
+        print(text1)
+        print(text2)
+        # df = pd.DataFrame(file)
+        # plt.scatter(df['text1'])
+      
+        plt.figure(num=1, figsize=(8, 6))
+        plt.title('Plot 1', size=14)
+        plt.xlabel('x-axis', size=14)
+        plt.ylabel('y-axis', size=14)
+        plt.plot(file['Phone'], file['School'])
+        plt.hist(file['City'])
+        #plt.plot(xData, yData2, color='r', linestyle='-', label='y2 data')
+        plt.legend(loc='upper left')
+        plt.savefig('plot1.png', format='png')
+        plt.show()
+
 
     def file_save(self):
         name = QtGui.QFileDialog.getSaveFileName(self, 'Save File')
@@ -89,6 +144,11 @@ class Window(QtGui.QMainWindow):
     def editor(self):
         self.textEdit = QtGui.QTextEdit()
         self.setCentralWidget(self.textEdit)
+        btn = QtGui.QPushButton("Quit", self)
+        btn.clicked.connect(self.close_application)
+        btn.resize(btn.minimumSizeHint())
+        btn.move(100,100)
+
 
 
 
